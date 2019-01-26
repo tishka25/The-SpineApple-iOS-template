45dd69c00ec2162f39fa411edfc285df2524dc4b
Tedko <teodorstanishev@gmail.com>
2019-01-26 12:31:29 +0200

    Initial Commit

---
+++ The SpineApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The SpineApp
+//
+//  Created by Teodor Stanishev on 26.01.19.
+//  Copyright © 2019 THESPINEAPPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
